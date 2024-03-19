4372f677f79a858f7c4cc6b0a420f1bad83725f3
Madison Saylor <maddie0264@gmail.com>
2024-03-19 19:21:04 -0400

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+
+// initialize
+const app = express();
+
+// enable/disable cross origin resource sharing if necessary
+app.use(cors());
+
+// enable/disable http request logging
+app.use(morgan('dev'));
+
+// enable only if you want templating
+app.set('view engine', 'ejs');
+
+// enable only if you want static assets from folder static
+app.use(express.static('static'));
+
+// this just allows us to render ejs from the ../app/views directory
+app.set('views', path.join(__dirname, '../src/views'));
+
+// enable json message body for posting data to API
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // To parse the incoming requests with JSON payloads
+
+// additional init stuff should go before hitting the routing
+
+// default index route
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+// START THE SERVER
+// =============================================================================
+async function startServer() {
+  try {
+    // connect db
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/kahootAPI';
+    await mongoose.connect(mongoURI);
+    console.log(`Mongoose connected to: ${mongoURI}`);
+
+    const port = process.env.PORT || 9090;
+    app.listen(port);
+
+    console.log(`Listening on port ${port}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+startServer();
